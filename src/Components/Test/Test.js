fc7fd6aa4cb16880937b2cd7917e652f382c41b7
hpbdmylove <iammaytinhdibo@gmail.com>
2018-05-06 00:25:04 +0700

    ds

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -6,7 +6,7 @@ class Test extends React.Component {
     constructor(props, context) {
         super(props, context);
         console.log(props);
-     }
+    }
     render() {
         return (
             <div className='test-com'>
@@ -16,8 +16,10 @@ class Test extends React.Component {
                         <button><span>&#8594;</span></button>
                     </div>
                     <div className="input">
-                     <input value={window.location.pathname}/>
-                     </div>
+                        <input value={window.location.pathname} />
+                    </div>
+
+                    <button className="reload"><span>&#8634;</span></button>
 
                 </div>
                 <TestClass />
