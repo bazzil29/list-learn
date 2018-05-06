45c9023d458996a15043017a84d4f8e0be87c645
PPbra <16021629@vnu.edu.vn>
2018-05-06 09:38:02 +0700

    push src

--- src/Components/Test/TestApp.js
+++ src/Components/Test/TestApp.js
@@ -1,12 +1,29 @@
-import React from "react";
-import {Switch, Route} from "react-router-dom";
+import React, { Component } from 'react';
+import {Switch, Route ,Link} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
-class Test  extends React.Component {
-    render(){
-        return(
-         <h2>Hello world</h2>
-        )
-    }
+
+class TestClass extends React.Component {
+  render(){
+    return (
+      <div>
+        <Link to={'/modal'}>Mở popup</Link>
+        <Switch>
+            <Route exact path = {'/modal'} component={Modal}/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
-export default Test;
+class Modal extends React.Component {
+  render(){
+    return (
+      <div style={{"background":"red","height":"200px"}}>
+        <Link to={'/popup'}>Đóng</Link>
+      </div>
+    )
+  }
+}
+
+export default TestClass
